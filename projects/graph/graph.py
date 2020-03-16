1651ad1d9306dda646c8da5c07039441adf0a0e0
Brenda Joshua <brendajosh.bj@gmail.com>
2020-03-16 17:59:15 +0300

    create add_vertex and add_edge

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,26 +13,48 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add[v2]
+        else:
+            raise IndexError("That vertex does not exist")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        #create an empty queue
+        q = Queue()
+        # add the starting vertex_id to the queue
+        q.enqueue(starting_vertex)
+        # create an empty set to store visited nodes
+        visited = set()
+        # while the queue is not empty...
+        print("\n\nbft")
+        while q.size() > 0:
+            # dequeue the first vertex
+            v = q.dequeue()
+            # check if it's been visited
+            # if not been visited...
+            if v not in visited:
+                # mark it as visited
+                print(v)
+                visited.add(v)
+                # then add all neighbors to the back of the queue
+                for neighbor in self.get_neighbors(v):
+                    q.enqueue(neighbour)
 
     def dft(self, starting_vertex):
         """
